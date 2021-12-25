112e29f1c91ec12545f4b9411d77b27f6668571c
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-25 18:30:41 +0100

    Header

--- src/header.js
+++ src/header.js
@@ -13,7 +13,13 @@ export default function renderHeader() {
     const menu = document.createElement('ul');
     const item1 = document.createElement('li');
     item1.innerHTML = '<a href="#" class="menuLink">Item 1</a>';
+    const item2 = document.createElement('li');
+    item2.innerHTML = '<a href="#" class="menuLink">Item 2</a>';
+    const item3 = document.createElement('li');
+    item3.innerHTML = '<a href="#" class="menuLink">Item 2</a>';
     menu.appendChild(item1);
+    menu.appendChild(item2);
+    menu.appendChild(item3);
     navMenu.appendChild(menu);
     container.appendChild(navMenu);
 
