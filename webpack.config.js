2fbbc1f0d0e19a9a4c94b679eae76cfc1d524137
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-25 16:56:08 +0100

    Wrote webpack config

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(jpg|jpeg|png|svg)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(ttf|otf)$/i,
+                type: 'asset/resource',
+            },
+        ]
+    }
+}
