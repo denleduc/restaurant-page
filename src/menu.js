57564a83251df29ee526e03c31b005ac029d34c8
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-26 02:36:08 +0100

    Dishes are displayed on the menu page
    I'm sure I could write better code for this though

--- src/menu.js
+++ src/menu.js
@@ -22,16 +22,90 @@ export default function renderMenu() {
     //Actual menu
     const menuContainer = document.createElement('div');
     menuContainer.classList.add('menuContainer');
-    console.log(dishes["Main dishes"]);
+
+    //Main dishes
+    const mainDishesContainer = document.createElement('div');
     dishes["Main dishes"].forEach(dish => {
         const dishDiv = document.createElement('div');
         dishDiv.classList.add('dish');
+
+        //Dish name
         const dishName = document.createElement('p');
         dishName.innerText = dish.name;
         dishDiv.appendChild(dishName);
 
-        menuContainer.appendChild(dishDiv);
+        //Dish description
+        const dishDesc = document.createElement('p');
+        dishDesc.innerText = dish.description;
+        dishDiv.appendChild(dishDesc);
+
+        //Ingredients
+        const dishIngredients = document.createElement('p');
+        dishIngredients.innerText = 'Ingredients: ';
+        dish.ingredients.forEach(ingredient => {
+            dishIngredients.innerText += ingredient + ' | ';
+        });
+        dishDiv.appendChild(dishIngredients);
+
+        mainDishesContainer.appendChild(dishDiv);
     });
+    menuContainer.appendChild(mainDishesContainer);
+
+    //Desserts
+    const dessertsContainer = document.createElement('div');
+    dishes["Desserts"].forEach(dish => {
+        const dishDiv = document.createElement('div');
+        dishDiv.classList.add('dish');
+
+        //Dish name
+        const dishName = document.createElement('p');
+        dishName.innerText = dish.name;
+        dishDiv.appendChild(dishName);
+
+        //Dish description
+        const dishDesc = document.createElement('p');
+        dishDesc.innerText = dish.description;
+        dishDiv.appendChild(dishDesc);
+
+        //Ingredients
+        const dishIngredients = document.createElement('p');
+        dishIngredients.innerText = 'Ingredients: ';
+        dish.ingredients.forEach(ingredient => {
+            dishIngredients.innerText += ingredient + ' | ';
+        });
+        dishDiv.appendChild(dishIngredients);
+
+        dessertsContainer.appendChild(dishDiv);
+    });
+    menuContainer.appendChild(dessertsContainer);
+
+    //Others
+    const otherContainer = document.createElement('div');
+    dishes["Others"].forEach(dish => {
+        const dishDiv = document.createElement('div');
+        dishDiv.classList.add('dish');
+
+        //Dish name
+        const dishName = document.createElement('p');
+        dishName.innerText = dish.name;
+        dishDiv.appendChild(dishName);
+
+        //Dish description
+        const dishDesc = document.createElement('p');
+        dishDesc.innerText = dish.description;
+        dishDiv.appendChild(dishDesc);
+
+        //Ingredients
+        const dishIngredients = document.createElement('p');
+        dishIngredients.innerText = 'Ingredients: ';
+        dish.ingredients.forEach(ingredient => {
+            dishIngredients.innerText += ingredient + ' | ';
+        });
+        dishDiv.appendChild(dishIngredients);
+
+        otherContainer.appendChild(dishDiv);
+    });
+    menuContainer.appendChild(otherContainer);
 
     container.appendChild(menuContainer);
     return container;
