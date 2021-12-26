63a7bd7aae6bcc50e87a07b1f93e9000202164b5
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-26 15:43:54 +0100

    Finished
    Added a readme too

---
+++ README.md
@@ -0,0 +1,5 @@
+# Restaurant page
+This repository contains a restaurant landing page created while following 'The Odin Project'. It has been built using webpack. Its purpose is to practice DOM manipulation (the entire page is rendered using JavaScript).
+
+## Live demo
+Available on: [GitHub pages](#)
--- src/about.js
+++ src/about.js
@@ -1,4 +1,3 @@
-import license from './OFL.txt';
 import kitchenImg from './kitchen.jpg';
 
 export default function renderAbout() {
@@ -25,6 +24,10 @@ export default function renderAbout() {
     kitchen.classList.add('kitchenImg');
     aboutContainer.appendChild(kitchen);
 
+    const findUsTxt = document.createElement('p');
+    findUsTxt.innerText = 'Want to test ? Great! You can find us at "insert the name of a street of the city you live in"!'
+    aboutContainer.appendChild(findUsTxt);
+
 
     //Credits
     const creditsContainer = document.createElement('div');
@@ -40,19 +43,19 @@ export default function renderAbout() {
     const credit5 = document.createElement('p');
     const credit3 = document.createElement('p');
     const credit4 = document.createElement('p');
+    const credit6 = document.createElement('p');
     credit1.innerHTML = 'Restaurant photo by <a href="https://unsplash.com/@ninjason?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Jason Leung</a> on <a href="https://unsplash.com/s/photos/restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>';
     credit2.innerHTML = 'Background photo by <a href="https://unsplash.com/@danielcgold?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dan Gold</a> on <a href="https://unsplash.com/s/photos/food?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>';
     credit5.innerHTML = 'Kitchen photo by <a href="https://unsplash.com/@michaelwb?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Michael Browning</a> on <a href="https://unsplash.com/s/photos/kitchen-restaurant?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>';
     credit3.innerHTML = 'Cantarell font from Google Fonts. Licensed under the SIL Open Font License, Version 1.1';
     credit4.innerHTML = 'Licorice font from Google Fonts. Licensed under the SIL Open Font License, Version 1.1';
+    credit6.innerHTML = 'This license is available in the source code (OFL.txt file) and with a FAQ at: <a href="http://scripts.sil.org/OFL">http://scripts.sil.org/OFL</a>';
     creditsContainer.appendChild(credit1);
     creditsContainer.appendChild(credit2);
     creditsContainer.appendChild(credit5);
     creditsContainer.appendChild(credit3);
     creditsContainer.appendChild(credit4);
-    const licenseText = document.createElement('p');
-    licenseText.innerText = license;
-    creditsContainer.appendChild(licenseText);
+    creditsContainer.appendChild(credit6);
 
     container.appendChild(aboutContainer);
     container.appendChild(creditsContainer);
