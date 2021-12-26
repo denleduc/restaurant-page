1120c683c395868de81d651148aee04dfe194cc7
Denis <93516910+Bussun@users.noreply.github.com>
2021-12-26 04:07:11 +0100

    oops
    I broke tabs so I fixed them

--- src/header.js
+++ src/header.js
@@ -13,15 +13,15 @@ export default function renderHeader() {
     navMenu.classList.add('nav')
 
     const item1 = document.createElement('button');
-    item1.classList.add('btn');
+    item1.classList.add('presBtn', 'btn');
     item1.innerText = 'Presentation';
 
     const item2 = document.createElement('button');
-    item2.classList.add('btn');
+    item2.classList.add('menuBtn', 'btn');
     item2.innerText = 'Menu';
 
     const item3 = document.createElement('button');
-    item3.classList.add('btn');
+    item3.classList.add('aboutBtn', 'btn');
     item3.innerText = 'About us';
     
     navMenu.appendChild(item1);
